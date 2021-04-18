a8f39d180cc865cee6295e08e7eb6b71db41f9f8
Will Miller <willmiller@Wills-Air.fios-router.home>
2021-04-18 09:18:36 -0400

    basic conditionals added

--- TripleX.cpp
+++ TripleX.cpp
@@ -7,7 +7,7 @@ int main()
 
     std::cout << "You are a Detective trying to solve the mystery"; //std = namespace , :: = scope operator, cout (definied in std namespace)
     std::cout << std::endl;
-    std::cout << "The killer leaves increasingly difficult combinations of numbers";
+    std::cout << "The thief leaves increasingly difficult combinations of numbers";
     
 
     //declare variables
@@ -27,6 +27,28 @@ int main()
     std::cout << "The code's product is: " << CodeProduct << std::endl;
 
     std::cout << "Enter the correct code to continue to the next clue" <<std::endl;
+
+
+    // declare a variable for the plater's guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+    // std::cout << "You entered: " << GuessA << GuessB <<GuessC;
+
+
+    //declare variables for guess sum and product
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProd = GuessA * GuessB * GuessC;
+
+    //if statement
+    if (GuessSum == CodeSum && GuessProd == CodeProduct)
+    {
+        std::cout << "You solved the puzzle!";
+    } else {
+        std::cout << "Sorry that is the wrong answer. The thief got away..."
+    }
+    
 
     return 0;
 }
